21c5bfd64cbf107be94ebf2cb1744b86dccfa1ef
gmorikawa <gabriel_morikawa@yahoo.co.jp>
2019-03-02 20:50:09 -0300

    Adicionado cena menu inicial

---
+++ Assets/Scripts/MenuInicial.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Cenario1");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void HoverEffect(Transform button)
+    {
+        button.localScale = new Vector3(1.2f, 1.2f);
+    }
+
+    public void UnhoverEffect(Transform button)
+    {
+        button.localScale = new Vector3(1f, 1f);
+    }
+}
